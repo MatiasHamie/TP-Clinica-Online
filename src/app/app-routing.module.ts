425392c0a138510007629a2b176e1d380f27c4d6
MatiasHamie <mnhamie@gmail.com>
2020-10-22 19:03:31 -0300

    prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,12 @@ import { RegistroComponent } from './componentes/usuario/registro/registro.compo
 import { VerificarLoginGuard } from "./componentes/compartido/guards/verificar-login.guard";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'home', component: BienvenidoComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'login', component: LoginComponent },
   // { path: 'error', component: ErrorComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', component: ErrorComponent }
 ];
 
 @NgModule({
