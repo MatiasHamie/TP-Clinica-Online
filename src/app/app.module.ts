02539ea18d999b5677d174139b4ef4aaef5275ea
MatiasHamie <mnhamie@gmail.com>
2020-10-22 13:22:48 -0300

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/usuario/login/login.component';
+import { RegistroComponent } from './componentes/usuario/registro/registro.component';
+import { ErrorComponent } from './componentes/paginas/error/error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
